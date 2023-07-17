bd63b54773cc6b38f3905dd2f2cdcee9b5068ba9
AndrewLim8140 <andrewfeilong.lim@my.jcu.edu.au>
2023-07-17 17:32:58 +0800

    Add prac 1 files

--- prac_01/broken_score.py
+++ prac_01/broken_score.py
@@ -3,15 +3,11 @@ score = float(input("Enter score: ")
               )
 # if number isnt within 0 to 100
 if score > 100 or score < 0:
-
-print("Invalid Score")
+    print("Invalid Score")
 # computation of scores
 elif score > 90:
-
-print("Excellent")
+    print("Excellent")
 elif score > 50:
-
-print("Passable")
+    print("Passable")
 else:
-
-print("Bad")
+    print("Bad")
