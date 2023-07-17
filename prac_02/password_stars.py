d7c95f5266af8b4b5cbe9cfa33b19721c24f77bb
AndrewLim8140 <andrewfeilong.lim@my.jcu.edu.au>
2023-07-17 17:38:08 +0800

    Refractor password check program to utilise functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -5,17 +5,31 @@ check if meet min length
 if not met reinput
 if met output pw as *
 """
+global MINIMUM_LENGTH
 
 MINIMUM_LENGTH = 6
 password_length = 0
 
-while password_length < MINIMUM_LENGTH:
+def main(password_length):
+    while password_length < MINIMUM_LENGTH:
+        password_length = get_password(password_length)
+        if password_length >= MINIMUM_LENGTH:
+            censor_password(password_length)
+            break
+        else:
+            print("password length is too short")
+
+
+def censor_password(password_length):
+    print("*" * password_length)
+
+
+def get_password(password_length):
     print("Please input the desired password")
-    print("Password must be ",MINIMUM_LENGTH," characters or more")
+    print("Password must be ", MINIMUM_LENGTH, " characters or more")
     user_password = input(">>>")
     password_length = len(user_password)
-    if password_length >=MINIMUM_LENGTH:
-        print("*"*password_length)
-        break
-    else:
-        print("password length is too short")
+    return password_length
+
+
+main(password_length)
