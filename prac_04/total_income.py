e14c5950c2d646c4f2912a9e5f6d0f380f2a6120
AndrewLim8140 <andrewfeilong.lim@my.jcu.edu.au>
2023-07-31 17:26:39 +0800

    Adding subject_reader.py raw

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -1,24 +1,44 @@
 """
 CP1404/CP5632 Practical
 Starter code for cumulative total income program
+
+want: cal monthly cumulative total
+
+require:
+get no of monthly income
+get each mth income
+store in list
+display list of mth income and cumulative total
 """
 
 
 def main():
-    """Display income report for incomes over a given number of months."""
-    incomes = []
-    months = int(input("How many months? "))
+    """Display income report for incomes over a given number of number_of_month."""
+    incomes,monthly_totals = [],[]
+    number_of_month = int(input("How many number_of_month? "))
 
-    for month in range(1, months + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
+    for month in range(1, number_of_month + 1):
+        income = float(input(f"Enter income for month {str(month)} : "))
         incomes.append(income)
 
     print("\nIncome Report\n-------------")
+    print_report(incomes, number_of_month,monthly_totals)
+
+
+    for month in range(1,number_of_month + 1):
+        income = incomes[month-1]
+        current_total = monthly_totals[month-1]
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, current_total))
+
+
+def print_report(incomes, number_of_month,monthly_totals):
     total = 0
-    for month in range(1, months + 1):
+    for month in range(1, number_of_month + 1):
         income = incomes[month - 1]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        monthly_totals.append(total)
+        print(monthly_totals)
+    return(monthly_totals)
 
 
 main()
