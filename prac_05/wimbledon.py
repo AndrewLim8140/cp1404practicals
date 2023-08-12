33c21c217ba1a353cd21f433c4326e45633ba0a0
AndrewLim8140 <andrewfeilong.lim@my.jcu.edu.au>
2023-08-12 12:01:20 +0800

    refactoring code to utlise functions

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -5,50 +5,64 @@ get winning country
 sort
 print
 """
-champions_dic = {}
-countries_dic = {}
-winning_countries = ''
-Header_removed = False
+champions_dic={}
+countries_dic={}
+winning_countries=''
+Header_removed=False
 
-with open('wimbledon.csv', 'r', encoding='utf-8-sig') as in_file:
-    for data in in_file:
-        if Header_removed == True:
-            data = data.split(',')
+def main(Header_removed,winning_countries):
 
-            # obtain champs
-            champion = data[2]
+    with open('wimbledon.csv','r',encoding='utf-8-sig') as in_file:
+        for data in in_file:
+            if Header_removed == True:
+                data=data.split(',')
 
-            try:
-                count_champions = champions_dic[champion]
-                count_champions += 1
-                champions_dic[champion] = count_champions
+                #obtain champs
+                champion=data[2]
+                champions_counter(champion)
 
-            except KeyError:
-                champions_dic[champion] = 1
+                #obtain country
+                country=data[1]
+                countries_counter(country)
 
-            # obtain country
-            country = data[1]
+            if Header_removed == False:
+                Header_removed=True
 
-            try:
-                count_country = countries_dic[country]
-                count_country += 1
-                countries_dic[country] = count_country
+        printing_champions()
 
-            except KeyError:
-                countries_dic[country] = 1
+        print("")
+        print(f'These {len(countries_dic)} countries have won Wimbledon :')
+        winning_countries=combining_countries_to_str(winning_countries)
+        print(f'{winning_countries}')
 
-        if Header_removed == False:
-            Header_removed = True
 
+def champions_counter(champion):
+    try:
+        count_champions = champions_dic[champion]
+        count_champions += 1
+        champions_dic[champion] = count_champions
+
+    except KeyError:
+        champions_dic[champion]=1
+
+def countries_counter(country):
+    try:
+        count_country = countries_dic[country]
+        count_country += 1
+        countries_dic[country] = count_country
+
+    except KeyError:
+        countries_dic[country]=1
+
+def printing_champions():
     print('Wimbledon Champions :')
     for champion in champions_dic:
         count = champions_dic[champion]
         print(f'{champion}  {count}')
 
-    print("")
-    print(f'These {len(countries_dic)} countries have won Wimbledon :')
-    countries = list(countries_dic.keys())
-    for country in countries:
-        winning_countries = winning_countries + country + ", "
-    print(f'{winning_countries}')
-
+def combining_countries_to_str(winning_countries):
+        countries=list(countries_dic.keys())
+        for country in countries:
+            winning_countries = winning_countries+country+", "
+        return winning_countries
+main(Header_removed,winning_countries)
